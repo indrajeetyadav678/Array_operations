9cb3f63df50c8e76b5155f03ba975c84fe8e2760
Indrajeet <yindrajeet678@gmail.com>
2024-03-09 05:54:12 +0530

    C++ Array operations

---
+++ Array.cpp
@@ -0,0 +1,22 @@
+//Array ===> size(),at(),front(),back() fill(),swap(), empty()
+// 1. vector =>push_back(), pop_back
+// 2. pair =>
+// 3. Tupple =>
+// 4. stack =>push(), pop(),top(),empty(),size()
+// 5. Queue => push(),pop(), front(),empty(),size(),back()
+// 6. Deque => push_back,push_front(),pop_back(),pop_front(),front(),back(),size()
+
+/*7. Forward_list (singly linked_list)            8. list(Duobly linked_list)
+=>push_front(),insert_after(),                   =>push_back(),push_front(),insert()
+=>pop_front(),remove_if(),remove(),clear()       =>pop_back(),pop_front(),
+=>unique(), merge(),  sort(), reverse(), 
+=>splice_after(),distance(), assign()
+*/
+// 9. map
+// 10. Unorderd Map
+// 11. Set
+// 12.Unorderd Set
+
+//functions
+
+
